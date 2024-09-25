eaa2cacf14182abee39b4c7a060bc3289c6d5566
ksmratih <ratihhanindyani@gmail.com>
2024-09-25 11:34:16 +0700

    tutorial 4

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
-from django.shortcuts import render, redirect   # Add import redirect at this line
+from django.shortcuts import render, redirect, reverse   # Add import redirect at this line
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.contrib.auth.forms import UserCreationForm
@@ -89,3 +89,26 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_mood(request, id):
+    # Get mood entry based on id
+    mood = MoodEntry.objects.get(pk = id)
+
+    # Set mood entry as an instance of the form
+    form = MoodEntryForm(request.POST or None, instance=mood)
+
+    if form.is_valid() and request.method == "POST":
+        # Save form and return to home page
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_mood.html", context)
+
+def delete_mood(request, id):
+    # Get mood based on id
+    mood = MoodEntry.objects.get(pk = id)
+    # Delete mood
+    mood.delete()
+    # Return to home page
+    return HttpResponseRedirect(reverse('main:show_main'))
